65cb919530013fd7e565936a071e4410ab0e8762
Neha Jha <Neha.Jha@Dell.com>
2023-02-21 14:06:11 +0530

    initial producerconsumer message

---
+++ Consumer/Receiver.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace Consumer
+{
+    public class Receiver
+    {
+        public static void Main(string[] args)
+        {
+            // Create a connection to the RabbitMQ server.
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    // Declare a queue to which you want to receive messages.
+                    channel.QueueDeclare(queue: "ReturnQueue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body.ToArray(); ;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine("Received message: {0}...", message);
+                    };
+
+                    channel.BasicConsume(queue: "ReturnQueue", autoAck: true, consumer: consumer);
+
+                    Console.WriteLine("Press [enter] to exit the Consumer App...");
+                    Console.ReadLine();
+                }
+
+            }
+
+
+
+
+        }
+    }
+}
---
+++ Producer/Sender.cs
@@ -0,0 +1,38 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace Producer
+{
+    public class Sender
+    {
+        public static void Main(string[] args)
+        {
+            // Create a connection to the RabbitMQ server.
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    // Declare a queue to which you want to send messages.
+                    channel.QueueDeclare(queue: "ReturnQueue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                    // Convert the message to a byte array.
+                    string message = "Getting started with RabbitMQ";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    // Publish the message to the queue.
+                    channel.BasicPublish(exchange: "", routingKey: "ReturnQueue", basicProperties: null, body: body);
+                    Console.WriteLine("Sent message: {0}...", message);
+                    
+                }
+
+            }
+            
+            Console.WriteLine("Press [enter] to exit the Sender App...");
+            Console.ReadLine();
+            
+
+        }
+    }
+}
